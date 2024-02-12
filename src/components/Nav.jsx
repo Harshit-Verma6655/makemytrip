cbe63b55f511b5fdadf5a69cd7acaf8c1787361c
Harshit-Verma6655 <144591839+Harshit-Verma6655@users.noreply.github.com>
2024-02-12 14:48:14 +0530

    updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,60 +1,68 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-
+import hotel from "../../assets/triplogos/hotel-1.png";
+import flight from "../../assets/triplogos/flight-1.png";
+import home from "../../assets/triplogos/villa-1.png";
+import holiday from "../../assets/triplogos/umbrella1.png";
+import train from "../../assets/triplogos/train-1.png";
+import bus from "../../assets/triplogos/bus-1.png";
+import cab from "../../assets/triplogos/car1.png";
+import forex from "../../assets/triplogos/forexcard-1.png";
+import travel from "../../assets/triplogos/travelinsu-1.png";
 function Nav() {
   return (
     <div className="w-screen flex justify-center absolute top-0 z-10 ">
       <div className="w-[928px] h-[110px]  bg-white px-6 pt-3 rounded-lg flex gap-12 text-sm shadow-lg">
         <div>
-          <img width={"62px"} src="../../assets/triplogos/flight-1.png" />
+          <img width={"62px"} src={flight} />
           <NavLink className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Flights
           </NavLink>
         </div>
         <div>
-          <img width={"52px"} src="../../assets/triplogos/hotel-1.png" />
+          <img width={"52px"} src={hotel} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Hotels
           </span>
         </div>
         <div>
-          <img width={"52px"} src="../../assets/triplogos/villa-1.png" />
+          <img width={"52px"} src={home} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Homestays & villas
           </span>
         </div>
         <div>
-          <img width={"42px"} src="../../assets/triplogos/umbrella1.png" />
+          <img width={"42px"} src={holiday} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Holiday packages
           </span>
         </div>
         <div>
-          <img width={"65px"} src="../../assets/triplogos/train-1.png" />
+          <img width={"65px"} src={train} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Trains
           </span>
         </div>
         <div>
-          <img width={"65px"} src="../../assets/triplogos/bus-1.png" />
+          <img width={"65px"} src={bus} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Buses
           </span>
         </div>
         <div>
-          <img width={"65px"} src="../../assets/triplogos/car1.png" />
+          <img width={"65px"} src={cab} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Cabs
           </span>
         </div>
         <div>
-          <img width={"42px"} src="../../assets/triplogos/forexcard-1.png" />
+          <img width={"42px"} src={forex} />
           <span className="mt-2 inline-block text-[#4a4a4a] text-[14px]">
             Forex Card & Currency
           </span>
         </div>
         <div>
-          <img width={"42px"} src="../../assets/triplogos/travelinsu-1.png" />
+          <img width={"42px"} src={travel} />
           <span className="mt-1 inline-block text-[#4a4a4a] text-[14px]">
             Travel Insurance
           </span>
