e05e67a1b7574db1f3eb643f9cdd2b102aa716ce
Harshit-Verma6655 <144591839+Harshit-Verma6655@users.noreply.github.com>
2024-02-27 23:35:04 +0530

    jj

--- src/Pages/SearchFlight.jsx
+++ src/Pages/SearchFlight.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Flight from '../components/Flight'
 import url from '../../assets/bg2.jpg';
+import { useNavigate } from 'react-router-dom';
 function SearchFlight() {
+    let navigate = useNavigate();
     return (
-        <div className={` flex flex-col justify-center  h-[100vh] bg-contain `} style={{ backgroundImage: `url(${url})`, }}>
-
+        <div className={` flex flex-col justify-center  h-[100vh] bg-contain text-white `} style={{ backgroundImage: `url(${url})`, }}>
+            <button className='pl-4 text-2xl font-bold px-4 bg-black w-fit' onClick={() => navigate("/")}> Back</button>
             <div className='sm:hidden  '><Flight /></div>
         </div>
     )
